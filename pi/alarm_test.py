61df3efdd557a9d5e8db18c2267bf1ee9f0f8324
George Badulescu <georgebadulescu@gmail.com>
2026-03-11 17:15:30 -0700

    Update README and alarm test new audio

--- pi/alarm_test.py
+++ pi/alarm_test.py
@@ -6,7 +6,7 @@ print("Alarm is starting")
 try:
     while True:
         print("ALARM!")
-        result = os.system('mpg123 -q -a hw:0,0 /home/george/the-overseer/alarm2.mp3')  #this is the command to run the alarm
+        result = os.system('mpg123 -q -a hw:0,0 /home/george/the-overseer/alarm_time.wav')  #this is the command to run the alarm
         #have to include this below bc mpg123 returns 0 on success and it keeps looping the alarm need to get out
         if result != 0:
             break
